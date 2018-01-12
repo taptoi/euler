5969c2d918b18f5a1bbcfd0fdc463886173894cb
taptoi <toivasentapani@gmail.com>
2018-01-12 14:48:46 +0100

    Update

--- ProjectEuler/ProjectEuler/P1.fs
+++ ProjectEuler/ProjectEuler/P1.fs
@@ -1,19 +1,7 @@
 ﻿namespace ProjectEuler
 
 module P1 =
-
-    let IsDividend x N = 
-        x % N = 0. && x <> 0.
-
-    let IsDividend3Or5 x =
-        IsDividend x 3. || IsDividend x 5.
-
-    let DividendsOf3Or5 N =
-        let seqN = seq { 0. .. 1. .. (float(N)-1.) }
-        seq { for x in seqN do 
-                match IsDividend3Or5 x with
-                | true -> yield int(x)
-                | false -> ()
-             }
-
-    let result = DividendsOf3Or5 1000 |> Seq.sum
+    let solve limit =
+        {1 .. limit}
+        |> Seq.filter (fun i -> i % 3 = 0 || i % 5 = 0)
+        |> Seq.sum
